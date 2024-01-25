023f0f6bb6f86ea3b095e9b6b3207aea1aed6669
SMHRD <SMHRD@222.102.104.98>
2024-01-25 11:36:43 +0900

    고현우 클래스 파일

---
+++ gitTest2/src/gitTest2/고현우.java
@@ -0,0 +1,17 @@
+package gitTest2;
+
+public class 고현우 {
+
+	public static void main(String[] args) {
+
+		System.out.println("고현우 파일");
+		
+		int num1 = 2;
+		int num2 = 3;
+		
+		System.out.println(num1+num2);
+		
+		
+	}
+
+}
