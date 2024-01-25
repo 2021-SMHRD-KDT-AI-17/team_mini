5b8b422cb4a12ef6dd5882c9ea55d7c88efa9610
smhrd <smhrd@121.147.52.249>
2024-01-25 11:41:30 +0900

    식단명 오류 수정 들깨칼국수 --> 들깨수제비

--- gitTest2/src/gitTest2/명택.java
+++ gitTest2/src/gitTest2/명택.java
@@ -4,7 +4,7 @@ public class 명택 {
 
 	public static void main(String[] args) {
 		System.out.println("24.01.25. 오늘의 식단");
-		System.out.println("제육볶음, 들깨칼국수");
+		System.out.println("제육볶음, 들깨수제비");
 
 	}
 
