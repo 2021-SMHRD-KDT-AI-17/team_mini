17474a9864ea0ed40a2467c57b0795e474a729e0
smhrd <smhrd@121.147.52.249>
2024-01-25 11:36:03 +0900

    1.25.그냥 밥집 식단

--- gitTest2/src/gitTest2/명택.java
+++ gitTest2/src/gitTest2/명택.java
@@ -3,7 +3,8 @@ package gitTest2;
 public class 명택 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("24.01.25. 오늘의 식단");
+		System.out.println("제육볶음, 들깨칼국수");
 
 	}
 
